8100672a1b0ea839cafb2cd1aa0756459e589dc4
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-26 16:02:42 -0200

    atualizado as funcoes da rede

---
+++ functions.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Pensando o Direito Network Functions
+Plugin URI: http://github.com/pensandoodireito/pensandoodireito-network-functions
+Description: Plugin para reunir as funções de rede da plataforma de debates do Pensando o Direito
+Author: Marco Antonio Konopacki
+Version: 0.1
+Author URI: http://github.com/marcoamarelo
+*/
+
+add_action('wp_logout', 'pensandoodireito_redirecionar_logout');
+/**
+ * Redirecionara para a capa depois do logout
+ */
+function pensandoodireito_redirecionar_logout() {
+    wp_redirect( site_url("/") );
+    exit();
+}
+
+add_action('login_form', 'pensandoodireito_redirecionar_login');
+/**
+ * Redirecionar para a capa depois do login
+ */
+function pensandoodireito_redirecionar_login() {
+    global $redirect_to;
+    if (!isset($_GET['redirect_to'])) {
+        $redirect_to = site_url("/");
+    }
+}
+
+add_action('wp_footer', 'pensandoodireito_login_modal');
+
+function pensandoodireito_login_modal() {
+?>
+    <div class="modal fade" id="wp-login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content login">
+                ...
+            </div>
+        </div>
+    </div>
+<?php
+}
+
+add_action( 'login_enqueue_scripts', 'pensandoodireito_login_logo' );
+/**
+ * Função para substituir o logo padrão da tela de login
+ */
+function pensandoodireito_login_logo() { ?>
+    <style type="text/css">
+        div#login h1 a {
+            background-image: url(<?php echo get_template_directory_uri(); ?>/images/pensandoodireito-login-logo.png);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
